9901cfb418b9a2d524e97affe55d7e54ecb41828
rodrigospm <rodrigo.perez@alumnos.ucsp.edu.pe>
2022-09-20 13:28:56 -0500

    actualizando ejercicio 10

--- project_euler/problem10.cpp
+++ project_euler/problem10.cpp
@@ -3,14 +3,28 @@ Find the sum of all the primes below two million. */
 
 #include <iostream>
 using namespace std;
+  
+bool esPrimo(int n) {
+    if (n == 1 || n == 0)
+        return false;
 
-int main() {
-
-    int i = 0;
-
-    while (i >= 10) {
-        
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0)
+            return false;
     }
+    return true;
+}
 
+int main()
+{
+    long long N = 2000000;
+    long long sum = 0;
+  
+    for (int i = 1; i <= N; i++) {
+        if (esPrimo(i)) {
+            sum = sum + i;
+        }
+    }
+    cout << sum;
     return 0;
 }
